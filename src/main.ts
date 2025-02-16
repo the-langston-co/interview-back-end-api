41261da99a9588dbde883284b736f0c1b0b65391
Neil Poulin <neil@neilpoulin.com>
2025-02-16 14:01:01 -0700

    update port, add logging in main setup script

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
+const logger = new Logger('main');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT ?? 3005;
+  logger.debug(`Starting server on http://localhost:${port}`)
+  await app.listen(port);
 }
 bootstrap();
